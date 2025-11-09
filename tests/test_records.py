fdcd8d1b8835b39b8c839fac453009e7fe64d98b
akahn <johcikahn@gmial.com>
2025-11-09 13:17:41 -0500

    review update

--- tests/test_records.py
+++
@@ -1,6 +0,0 @@
-from repocapsule.records import build_record
-
-
-def test_build_record_respects_precomputed_tokens():
-    rec = build_record(text="hello world", rel_path="doc.txt", tokens=123)
-    assert rec["meta"]["tokens"] == 123
